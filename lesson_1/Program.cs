dd52975fe1518c670867849809265c0c1b396557
User <organik07@mail.ru>
2022-10-09 20:29:00 +0300

    added lesson_1

---
+++ lesson_1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+string a = Console.ReadLine();
+string b = Console.ReadLine();
+int ai = int.Parse(a);
+int bi = int.Parse(b);
+Console.WriteLine(ai+bi);
+
