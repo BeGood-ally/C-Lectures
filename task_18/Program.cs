ef5499c80a9ec12533cf2d2c225e2bd2ca923bbc
User <organik07@mail.ru>
2022-10-09 22:06:13 +0300

    added task_18

---
+++ task_18/Program.cs
@@ -0,0 +1,10 @@
+﻿string a = Console.ReadLine();
+int ai = int.Parse(a);
+for (int i = 2; i <= ai; i = i + 2){
+    if (i < ai && i != ai - 1){
+        Console.Write(i+", ");
+    }
+    else if(i == ai || i == ai - 1){
+        Console.Write(i);
+    }
+}
