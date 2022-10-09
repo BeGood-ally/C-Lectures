301d1883981d985a07eaa525857aff608a707309
User <organik07@mail.ru>
2022-10-09 21:36:47 +0300

    added task_16

---
+++ task_16/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+string a = Console.ReadLine();
+int ai = int.Parse(a);
+switch(ai%2){
+    case 0:
+        Console.WriteLine("число четное");
+        break;
+    case 1:
+        Console.WriteLine("число нечетное");
+        break;
+}
