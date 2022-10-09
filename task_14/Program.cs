37eb5b005e5f743e2786bbde3ed6de550090ebd1
User <organik07@mail.ru>
2022-10-09 21:22:48 +0300

    added task_14

---
+++ task_14/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+string a = Console.ReadLine();
+string b = Console.ReadLine();
+string c = Console.ReadLine();
+int ai = int.Parse(a);
+int bi = int.Parse(b);
+int ci = int.Parse(c);
+if (ai <= bi){
+    if (bi >= ci){
+        Console.WriteLine("max "+bi);
+    }
+    if (bi < ci){
+        Console.WriteLine("max "+ci);
+    }
+    }
+if (ai > bi) {
+    Console.WriteLine("max "+ai);
+}
+
