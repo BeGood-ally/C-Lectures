92e14f5266213cd3c53e05abd76933ccf886d572
User <organik07@mail.ru>
2022-10-12 22:26:42 +0300

    added task_319 without '-'

---
+++ lesson_3/task_319/Program.cs
@@ -0,0 +1,26 @@
+﻿string str = "12351";
+if (str.Length == 5){
+    try {
+        int strNum = int.Parse(str);
+        string answer = "yes";
+        for (int i = 0; i < 2; i++){
+            if (str[i] != str[(str.Length - 1) - i]){
+                answer = "no";
+                break;
+            }
+            else continue;
+        }
+        switch(answer){
+            case "yes":
+                Console.Write("yes");
+                break;
+            case "no":
+                Console.Write("no");
+                break;
+        }
+    }
+    catch(System.FormatException){
+       Console.Write("введенная величина не является числом"); 
+    }
+}
+else Console.Write("число не пятизначное");
