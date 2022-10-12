f35467391535767d15234eb39cf900fdd416a104
User <organik07@mail.ru>
2022-10-12 23:15:38 +0300

    added task_321 without input

---
+++ lesson_3/task_321/Program.cs
@@ -0,0 +1,9 @@
+﻿int [] arrA = {2,4,6};
+int [] arrB = {1,2,3};
+double summ = 0;
+for (int i = 0; i < 3; i++){
+    summ = summ + Math.Pow((arrA[i] - arrB[i]), 2);
+}
+double mileage = Math.Round(Math.Sqrt(summ), 2);
+Console.Write(mileage);
+
