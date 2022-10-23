921a2356e894d99a5a7e6dc25fc058678f8bd71d
User <organik07@mail.ru>
2022-10-23 22:30:51 +0300

    added lesson_6

---
+++ lesson_6/task_641/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+int amountOfAboveZero() {
+    List <int> abovezero = new List <int>();
+    Regex regex = new Regex("(-?[0-9]+)");
+    while (true){ 
+        Console.WriteLine("введите число или 'q' для завершения");
+        string ?num = Console.ReadLine();
+        if(regex.IsMatch(num) == true){
+            int sign = Math.Sign(int.Parse(num)); 
+            abovezero.Add(sign);
+        }
+        else if(num == "q"){
+            break;
+        }
+        else {
+            Console.WriteLine("вы ввели не число введите число");
+        }
+    }
+    int count = 0;
+    foreach (int plus in abovezero){ 
+        if(plus == 1) count++;
+    }
+    return count;
+}
+
+int rez = amountOfAboveZero();
+Console.WriteLine($"количество чисел больше нуля из введенных вами равно: {rez}");
---
+++ lesson_6/task_643/Program.cs
@@ -0,0 +1,32 @@
+﻿double [,] Lines = new double[2,2];
+string [] Coefficients = new string []{"'k'","'b'"};
+string [] LineNumbers = new string []{"первой","второй"};
+
+void fillMatrix(string CoefNum, string DirecNum, int i, int j){
+    double signInt;
+    while(true){
+        Console.WriteLine($"введите коэффициент {CoefNum} {DirecNum} прямой");
+        string sign = Console.ReadLine();
+        if(double.TryParse(sign, out signInt)){
+            Lines[i,j] = signInt;
+            break;
+        }
+        else Console.WriteLine("вы ввели не число, введите численный коэффициент");
+    }
+}
+
+for (int i = 0; i < 2; i++){
+    for (int j = 0; j < 2; j++){
+       fillMatrix(Coefficients[j],LineNumbers[i],i,j); 
+    }
+}
+
+double x = (Lines[1,1] - Lines[0,1])/(Lines[0,0]-Lines[1,0]);
+double y = Lines[0,0]*x + Lines[0,1];
+
+Console.WriteLine($"точка пересечения имеет координаты: x = {x}, y = {y}");
+
+
+
+
+
