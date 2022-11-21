bea008528e0760da1fcdbe0fdc11d807b1e19d68
User <organik07@mail.ru>
2022-11-21 21:43:38 +0300

    added lesson_9

---
+++ lesson_9/task_964/Program.cs
@@ -0,0 +1,8 @@
+﻿string sequence(int a){
+    if (a > 1){
+       return $"{a}," + sequence(a - 1); 
+    }
+    else return "1";
+}
+
+Console.Write(sequence(9));
---
+++ lesson_9/task_966/Program.cs
@@ -0,0 +1,8 @@
+﻿int summa(int m, int n){
+    if(n == m){
+        return m;
+    }
+    else return n + summa(m, n - 1);
+}
+
+Console.Write(summa(3,7));
